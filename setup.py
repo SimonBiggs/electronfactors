9453a065612570b9fdb62bbbe9ad30f0865ffe3b
SimonBiggs <mail@simonbiggs.net>
2015-03-06 18:30:43 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='electronfactors',
+    version='0.1.0',
+    description='Electron factor equivalent ellipse spline modelling',
+    author='Simon Biggs',
+    author_email='mail@simonbiggs.net',
+    url='https://github.com/SimonBiggs/equivalent-ellipse-spline-modelling',
+    packages=[
+        'electronfactors',
+        'electronfactors._ellipseutilities',
+        'electronfactors._inpututilities'],
+     )

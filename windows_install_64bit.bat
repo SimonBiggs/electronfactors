68f324f4dede857260d9505a12f113232f28be2b
SimonBiggs <mail@simonbiggs.net>
2015-07-30 12:34:31 +1000

    revert directory name

--- windows_install_64bit.bat
+++ windows_install_64bit.bat
@@ -7,8 +7,8 @@ if /I "%c%" EQU "N" goto :skip_conda_install
 goto :choice
 
 :conda_install
-bitsadmin /transfer "MiniConda_download" https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe %~dp0\windows_install\Miniconda3-latest-Windows-x86_64.exe
-%~dp0\windows_install\Miniconda3-latest-Windows-x86_64.exe
+bitsadmin /transfer "MiniConda_download" https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe %~dp0\install\Miniconda3-latest-Windows-x86_64.exe
+%~dp0\install\Miniconda3-latest-Windows-x86_64.exe
 
 :skip_conda_install
-CALL %~dp0\windows_install\install_dependencies_64bit.bat
+CALL %~dp0\install\install_dependencies_64bit.bat

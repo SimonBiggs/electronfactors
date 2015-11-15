7baa1e6e79e6ec2dac30a91f2e7c43e0e9f15b7b
SimonBiggs <mail@simonbiggs.net>
2015-11-15 18:39:04 +1100

    pypi version number increased to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "electronfactors",
-    version = "0.1.3",
+    version = "0.1.4",
     author = "Simon Biggs",
     author_email = "mail@simonbiggs.net",
     description = "Predict electron insert factors",
